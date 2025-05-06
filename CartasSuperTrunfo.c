457a18ab660ae04afa7e558b2c6c18692cf5e5b7
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2025-05-06 21:49:56 +0000

    Foi criado as variáveis das duas cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+// Declaração das variáveis para armazenar os dados das duas cartas
+char estado1, estado2;  // Armazena a letra do estado para cada carta
+char codigoDaCarta1[3], codigoDaCarta2[3];  // Código da carta (2 caracteres + '\0')
+char nomeCidade1[50], nomeCidade2[50];  // Nome da cidade, até 49 caracteres
+int populacao1, populacao2;  // População das cidades
+float area1, area2;  // Área das cidades
+float pib1, pib2;  // PIB das cidades
+int numeroPontosTuristicos1, numeroPontosTuristicos2;  // Número de pontos turísticos
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
